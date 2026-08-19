d426c72bc0b6bfbfecca9278f21757c61531137a
Zach Karpinski <zach@karpilabs.io>
2026-08-18 21:35:45 -0400

    gradle bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.karpilabs.simplemp3"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
